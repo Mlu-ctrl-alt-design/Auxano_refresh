77bb6b00068ba0299a957f4ef82b600823c76578
Mlu Manda <mlumanda@gmail.com>
2023-05-27 23:45:00 +0200

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,46 @@
+import {
+  initPlasmicLoader,
+  PlasmicRootProvider,
+  PlasmicComponent,
+  ComponentRenderData
+} from '@plasmicapp/loader-react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { PLASMIC } from './plasmic-init';
 
-import Homepage from './components/Homepage';
-import Contact from './components/Contact'
-import Services from './components/Services'
-import About from './components/About';
-import { Routes,Route } from "react-router-dom"
-
-
-function App() {
- 
- return (
- 
- <Routes>
- 
-<Route path="/about" element={<About />}/>
-<Route path="/" element={<Homepage />} />
-<Route path="/services" element={<Services/>} />
-<Route path="/contact" element={<Contact/>} />
-
- </Routes>
- )
+function AppRoot() {
+  return (
+    <PlasmicRootProvider loader={PLASMIC}>
+      <Router>
+        <Routes>
+          <Route path="/" element={CatchAllPage()} />
+        </Routes>
+      </Router>
+    </PlasmicRootProvider>
+  );
 }
 
+// We try loading the Plasmic page for the current route.
+// If it doesn't exist, then return "Not found."
+export function CatchAllPage() {
+  const [loading, setLoading] = useState(true);
+  const [pageData, setPageData] = useState<ComponentRenderData | null>(null);
 
-export default App;
- 
+  useEffect(() => {
+    async function load() {
+      const pageData = await PLASMIC.maybeFetchComponentData(location.pathname);
+      setPageData(pageData);
+      setLoading(false);
+    }
+    load();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (!pageData) {
+    return <div>Not found</div>;
+  }
+  // The page will already be cached from the `load` call above.
+  return <PlasmicComponent component={location.pathname} />;
+}
